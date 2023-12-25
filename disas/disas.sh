02f71dfdb0d5b3ae40f45da9c79cf40da9347a4a
Jakob Kirsch <jakob.kirsch@web.de>
2023-12-25 19:51:53 +0100

    add mcu labels

--- disas/disas.sh
+++ disas/disas.sh
@@ -2,5 +2,5 @@
 cd "$(dirname $0)/.."
 
 for i in prio pro plus; do
-    python3 pyu8disas/main.py -l disas/labels_$i -o disas/$i.asm roms/$i.bin
+    python3 pyu8disas/main.py -l disas/labels_$i disas/labels_mcu -o disas/$i.asm roms/$i.bin
 done;
